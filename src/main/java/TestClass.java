41ca38d50a1358947c424859453a9265bfcf9e4e
terma <artem.stasuk@gmail.com>
2016-04-08 18:26:22 -0400

    Add test classes

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by terma on 4/8/16.
+ */
+public class TestClass {
+
+    public void none() {
+
+    }
+
+}
---
+++ src/test/java/TestClassTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by terma on 4/8/16.
+ */
+public class TestClassTest {
+
+    @Test
+    public void c() {
+        new TestClass().none();
+    }
+
+}
